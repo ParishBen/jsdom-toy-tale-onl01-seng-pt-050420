791d3351fbc170bd191497d77f01ab59debc4913
ParishBen <ParishBen13@gmail.com>
2020-11-28 10:40:30 -0500

    commit tree ce67691a4cff08c6b3bda3b25ee2b6b71431d755

--- src/index.js
+++ src/index.js
@@ -13,3 +13,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+// Add Toy Info to the Card
+
+// Each card should have the following child elements:
+
+//     h2 tag with the toy's name
+//     img tag with the src of the toy's image attribute and the class name "toy-avatar"
+//     p tag with how many likes that toy has
+//     button tag with a class "like-btn"
+//     <div class="card">
+//     <h2>Woody</h2>
+//     <img src=toy_image_url class="toy-avatar" />
+//     <p>4 Likes </p>
+//     <button class="like-btn">Like <3</button>
+//   </div>
+  
